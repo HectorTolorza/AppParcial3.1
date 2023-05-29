4216ccb0506a0f631732c32268f59f1e4fa20d97
HectorTolorza <113229742+HectorTolorza@users.noreply.github.com>
2023-05-29 00:45:24 -0500

    second commit

---
+++ TuuDuu/app/src/main/java/com/example/agendatuuduu/ActualizarNota/Actualizar_Nota.java
@@ -0,0 +1,16 @@
+package com.example.agendatuuduu.ActualizarNota;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agendatuuduu.R;
+
+public class Actualizar_Nota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_nota);
+    }
+}
